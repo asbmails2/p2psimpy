0ae77097c45098312b686cf67a6b75fd59ffa272
biachiarelli <biachiarelli@gmail.com>
2020-01-18 12:40:43 -0300

    Ajustes das classes ao novo formato de métrica

--- tests/quality_pragmaitc/test_interpretation.py
+++ tests/quality_pragmaitc/test_interpretation.py
@@ -1,4 +1,4 @@
-from goald.quality.pragmatic.model.metric import Metrics
+from goald.quality.pragmatic.model.metric import CommonMetrics
 from goald.quality.pragmatic.model.interpretation import Interpretation
 from goald.quality.pragmatic.model.context import Context
 from goald.quality.pragmatic.model.qualityConstraint import QualityConstraint
@@ -7,8 +7,8 @@ from goald.quality.pragmatic.model.qualityConstraint import QualityConstraint
 def test_interpretation():
     interp = Interpretation()
     context = Context("C1")
-    metric = Metrics(('SECONDS', True))
-    qc =  QualityConstraint(context, metric, 15, 'LESS_THAN')
+    commonMetrics = CommonMetrics()
+    qc =  QualityConstraint(context, commonMetrics.SECONDS, 15, 'LESS_THAN')
 
     interp.addQualityConstraint(qc)
     map = interp.getContextDependentInterpretation()
