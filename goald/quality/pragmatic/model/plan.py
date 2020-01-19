c68c85ec3bd6a2f94df5fcf654a449de1f6edab5
biachiarelli <biachiarelli@gmail.com>
2020-01-19 19:27:04 -0300

    Plan pragmatics

---
+++ goald/quality/pragmatic/model/plan.py
@@ -0,0 +1,20 @@
+from goald.quality.pragmatic.model.task import Task
+
+class Plan:
+    def __init__(self):
+        self.tasks = []
+    
+    def __init__(self, task):
+        self.tasks = []
+        self.tasks.append(task)
+
+    def add(self, plan):
+        for task in plan.getTasks():
+            if task not in self.tasks:
+                self.tasks.append(task)
+    
+    def addTask(self, task):
+        self.tasks.append(task)
+
+    def getTasks(self):
+        return self.tasks
